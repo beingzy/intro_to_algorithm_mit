2ee670a0d1e7c1279b6b5ff88bc8d74b9551d377
Yi Zhang <yizhang@pinterest.com>
2018-01-03 22:17:02 -0800

    add test

--- heap.py
+++ heap.py
@@ -16,14 +16,19 @@ class Heap():
     def get_parent(self, idx):
         return self.get_node(_to_list_index(_from_list_index(idx)//2))
 
-    def get_child(self, idx):
-        left_child_idx = 2 * _from_list_index(idx)
-        right_child_idx = 2 * _from_list_index(idx) + 1
+    def get_left_child(self, idx):
         total_items = len(self._alist)
+        left_child_idx = 2 * _from_list_index(idx)
         left_child = self.get_node(_to_list_index(left_child_idx))
-        right_child = self.get_node(_to_list_index(right_child_idx))
+        return left_child
 
-        return left_child, right_child
+    def get_right_child(self, idx):
+        right_child_idx = 2 * _from_list_index(idx) + 1
+        right_child = self.get_node(_to_list_index(right_child_idx))
+        return right_child
+
+    def get_children(self, idx):
+        return self.get_left_child(idx), self.get_right_child(idx)
 
 
 def _to_list_index(i):
@@ -32,3 +37,11 @@ def _to_list_index(i):
 
 def _from_list_index(i):
     return i + 1
+
+
+if __name__ == "__main__":
+    aheap = Heap([1, 2, 3, 4, 5, 6, 7, 8])
+    assert aheap.get_node(0) == 1
+    assert aheap.get_node(2) == 3
+    assert aheap.get_parent(2) == 1
+    assert aheap.get_children(2) == (6, 7)
