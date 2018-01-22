5700b9feb0a7ca9f4c91d558c7aff28f7cae9a79
Yi Zhang <yizhang@pinterest.com>
2018-01-21 22:22:29 -0800

    implement BFS method

--- graph/DFS.py
+++ graph/DFS.py
@@ -31,15 +31,32 @@ class Graph:
                 self.DFSUtil(i, visited)
 
     def DFS(self, v):
-        # visited = [False] * (len(self.graph))
         V = list(self.graph.keys())
         visited = dict(zip(
-            V,
-            [False] * len(V)))
+            V, [False] * len(V)))
 
         for i in V:
             if visited[i] == False:
                 self.DFSUtil(i, visited)
+
+    def BFS(self, s):
+        V = list(self.graph.keys())
+        visited = dict(zip(
+            V, [False] * len(V)))
+
+        frontier = []
+
+        frontier.append(s)
+        visited[s] = True
+
+        while frontier:
+            s = frontier.pop(0)
+            print(s)
+
+            for i in self.graph[s]:
+                if visited[i] == False:
+                    frontier.append(i)
+                    visited[i] = True
 
 
 if __name__ == "__main__":
@@ -53,3 +70,13 @@ if __name__ == "__main__":
 
     print("Following is DFS from (starting from vertex 2)")
     g.DFS(2)
+
+    g = Graph()
+    g.addEdge(0, 1)
+    g.addEdge(0, 2)
+    g.addEdge(1, 2)
+    g.addEdge(2, 0)
+    g.addEdge(2, 3)
+    g.addEdge(3, 3)
+    print("Following is BFS traversal (starting from vertex 2)")
+    g.BFS(2)
